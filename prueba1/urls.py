2f19a887e48749e41343104f395d773489c3044d
Germy <12211479verdozagerman@gmail.com>
2017-07-28 14:23:00 -0700

    Add files via upload

---
+++ prueba1/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'prueba1.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^app1/',include('App1.urls'),name='app1')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
